2f8fdabd62236f14d7573e70668d8044bc098fed
TienPerry <52100124@student.tdtu.edu.vn>
2023-04-04 00:32:58 +0700

    push recommit

---
+++ DrugStore/DAO/NCC_DAO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class NCC_DAO
+    {
+        private static NCC_DAO instance;
+        public static NCC_DAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new NCC_DAO();
+                }
+                return instance;
+            }
+
+        }
+        public NCC_DAO() { }
+
+        public DataTable getAllNCC()
+        {
+            return DataProvider.Instance.ExecuteQuery("SELECT MANCC, TENNCC FROM NhaCungCap");
+        }
+    }
+}
---
+++ DrugStore/DAO/NhanVienDAO.cs
@@ -0,0 +1,66 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class NhanVienDAO
+    {
+
+        private static NhanVienDAO instance;
+
+        public static NhanVienDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new NhanVienDAO();
+                }
+                return instance;
+            }
+
+        }
+
+        private NhanVienDAO()
+        {
+        }
+        public NhanVien checkAccount(String phone, String pass)    // Check whether exist an account of user to login
+        {
+            String query = "proc_Login @sodt , @pass";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query, new object[] { phone, pass });
+
+            if (data.Rows.Count == 0)
+            {
+                return null;
+            }
+
+            return new NhanVien(data.Rows[0]); // return an object KhachHang if exists in database 
+        }
+        public DataTable getAllAccount()
+        {
+            return DataProvider.Instance.ExecuteQuery("SELECT SDT, DIACHI, HOTEN, PASS FROM NhanVien");
+        }
+        public bool checkExistAccount(String phone)
+        {
+            String query = "SELECT * FROM NhanVien WHERE SDT = @phone";
+            DataTable data = DataProvider.Instance.ExecuteQuery(query, new object[] { phone });
+
+            if (data.Rows.Count == 0)
+            {
+                return false;
+            }
+
+            return true; // return an object NhanVien if exists in database
+        }
+        public bool insertAccountNhanVien(String sdt, String name, String diachi, String pass)
+        {
+            String query = string.Format("INSERT INTO NhanVien VALUES('{0}', N'{1}', N'{2}', '{3}')", sdt, name, diachi, pass);
+            return DataProvider.Instance.ExecuteNonQuery(query) > 0;
+        }
+    }
+}
