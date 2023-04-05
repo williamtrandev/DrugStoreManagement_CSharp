90405ce93e0d7d21b43eeb1922d44dbb7ab9012d
Sy Luan <52100061@student.tdtu.edu.vn>
2023-04-05 17:13:34 +0700

    phieunhaphang

---
+++ DrugStore/BUS/ChiTietNhapHangBUS.cs
@@ -0,0 +1,30 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class ChiTietNhapHangBUS
+    {
+        public static ChiTietNhapHangBUS instance;
+        public static ChiTietNhapHangBUS Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ChiTietNhapHangBUS();
+                }
+                return instance;
+            }
+        }
+        public ChiTietNhapHangBUS() { }
+        public bool insertChiTietNhapHang(String maphieu, String sodk, int soluong)
+        {
+            return DAO.ChiTietNhapHangDAO.Instance.insertChiTietNhapHang(maphieu, sodk, soluong);
+        }
+    }
+}
---
+++ DrugStore/DrugStore/FAddNV.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace DrugStore
+{
+    partial class FAddNV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FAddNV));
+            this.tb_sdt = new System.Windows.Forms.TextBox();
+            this.tb_tennv = new System.Windows.Forms.TextBox();
+            this.tb_diachi = new System.Windows.Forms.TextBox();
+            this.tb_matkhau = new System.Windows.Forms.TextBox();
+            this.pb_thongtinnv = new System.Windows.Forms.PictureBox();
+            this.pb_luu = new System.Windows.Forms.PictureBox();
+            this.pb_huy = new System.Windows.Forms.PictureBox();
+            this.pl_sdt = new System.Windows.Forms.Panel();
+            this.pn_tennv = new System.Windows.Forms.Panel();
+            this.pl_diachi = new System.Windows.Forms.Panel();
+            this.pl_matkhau = new System.Windows.Forms.Panel();
+            this.lb_validate_sdt = new System.Windows.Forms.Label();
+            this.lb_validate_ten = new System.Windows.Forms.Label();
+            this.lb_validate_diachi = new System.Windows.Forms.Label();
+            this.lb_validate_pass = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_thongtinnv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_luu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_huy)).BeginInit();
+            this.pl_sdt.SuspendLayout();
+            this.pn_tennv.SuspendLayout();
+            this.pl_diachi.SuspendLayout();
+            this.pl_matkhau.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb_sdt
+            // 
+            this.tb_sdt.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tb_sdt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.tb_sdt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_sdt.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_sdt.Location = new System.Drawing.Point(0, 0);
+            this.tb_sdt.Margin = new System.Windows.Forms.Padding(5);
+            this.tb_sdt.Name = "tb_sdt";
+            this.tb_sdt.Size = new System.Drawing.Size(430, 34);
+            this.tb_sdt.TabIndex = 4;
+            // 
+            // tb_tennv
+            // 
+            this.tb_tennv.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.tb_tennv.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_tennv.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_tennv.Location = new System.Drawing.Point(0, 0);
+            this.tb_tennv.Name = "tb_tennv";
+            this.tb_tennv.Size = new System.Drawing.Size(430, 34);
+            this.tb_tennv.TabIndex = 5;
+            // 
+            // tb_diachi
+            // 
+            this.tb_diachi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.tb_diachi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_diachi.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_diachi.Location = new System.Drawing.Point(0, 0);
+            this.tb_diachi.Name = "tb_diachi";
+            this.tb_diachi.Size = new System.Drawing.Size(430, 34);
+            this.tb_diachi.TabIndex = 6;
+            // 
+            // tb_matkhau
+            // 
+            this.tb_matkhau.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(222)))), ((int)(((byte)(89)))));
+            this.tb_matkhau.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_matkhau.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_matkhau.Location = new System.Drawing.Point(0, 0);
+            this.tb_matkhau.Name = "tb_matkhau";
+            this.tb_matkhau.Size = new System.Drawing.Size(430, 34);
+            this.tb_matkhau.TabIndex = 7;
+            // 
+            // pb_thongtinnv
+            // 
+            this.pb_thongtinnv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pb_thongtinnv.Image = ((System.Drawing.Image)(resources.GetObject("pb_thongtinnv.Image")));
+            this.pb_thongtinnv.Location = new System.Drawing.Point(0, 0);
+            this.pb_thongtinnv.Name = "pb_thongtinnv";
+            this.pb_thongtinnv.Size = new System.Drawing.Size(1010, 653);
+            this.pb_thongtinnv.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_thongtinnv.TabIndex = 10;
+            this.pb_thongtinnv.TabStop = false;
+            // 
+            // pb_luu
+            // 
+            this.pb_luu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb_luu.Image = ((System.Drawing.Image)(resources.GetObject("pb_luu.Image")));
+            this.pb_luu.Location = new System.Drawing.Point(650, 571);
+            this.pb_luu.Name = "pb_luu";
+            this.pb_luu.Size = new System.Drawing.Size(153, 50);
+            this.pb_luu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_luu.TabIndex = 11;
+            this.pb_luu.TabStop = false;
+            this.pb_luu.Click += new System.EventHandler(this.pb_luu_Click);
+            // 
+            // pb_huy
+            // 
+            this.pb_huy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pb_huy.Image = ((System.Drawing.Image)(resources.GetObject("pb_huy.Image")));
+            this.pb_huy.Location = new System.Drawing.Point(836, 571);
+            this.pb_huy.Name = "pb_huy";
+            this.pb_huy.Size = new System.Drawing.Size(153, 50);
+            this.pb_huy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_huy.TabIndex = 12;
+            this.pb_huy.TabStop = false;
+            this.pb_huy.Click += new System.EventHandler(this.pb_huy_Click);
+            // 
+            // pl_sdt
+            // 
+            this.pl_sdt.BackColor = System.Drawing.Color.Orange;
+            this.pl_sdt.Controls.Add(this.tb_sdt);
+            this.pl_sdt.Location = new System.Drawing.Point(310, 194);
+            this.pl_sdt.Name = "pl_sdt";
+            this.pl_sdt.Padding = new System.Windows.Forms.Padding(5);
+            this.pl_sdt.Size = new System.Drawing.Size(430, 40);
+            this.pl_sdt.TabIndex = 13;
+            // 
+            // pn_tennv
+            // 
+            this.pn_tennv.BackColor = System.Drawing.Color.Orange;
+            this.pn_tennv.Controls.Add(this.tb_tennv);
+            this.pn_tennv.Location = new System.Drawing.Point(310, 278);
+            this.pn_tennv.Name = "pn_tennv";
+            this.pn_tennv.Padding = new System.Windows.Forms.Padding(5);
+            this.pn_tennv.Size = new System.Drawing.Size(430, 40);
+            this.pn_tennv.TabIndex = 14;
+            // 
+            // pl_diachi
+            // 
+            this.pl_diachi.BackColor = System.Drawing.Color.Orange;
+            this.pl_diachi.Controls.Add(this.tb_diachi);
+            this.pl_diachi.Location = new System.Drawing.Point(310, 358);
+            this.pl_diachi.Name = "pl_diachi";
+            this.pl_diachi.Padding = new System.Windows.Forms.Padding(5);
+            this.pl_diachi.Size = new System.Drawing.Size(430, 40);
+            this.pl_diachi.TabIndex = 15;
+            // 
+            // pl_matkhau
+            // 
+            this.pl_matkhau.BackColor = System.Drawing.Color.Orange;
+            this.pl_matkhau.Controls.Add(this.tb_matkhau);
+            this.pl_matkhau.Location = new System.Drawing.Point(310, 441);
+            this.pl_matkhau.Name = "pl_matkhau";
+            this.pl_matkhau.Padding = new System.Windows.Forms.Padding(5);
+            this.pl_matkhau.Size = new System.Drawing.Size(430, 40);
+            this.pl_matkhau.TabIndex = 15;
+            // 
+            // lb_validate_sdt
+            // 
+            this.lb_validate_sdt.AutoSize = true;
+            this.lb_validate_sdt.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_validate_sdt.ForeColor = System.Drawing.Color.Red;
+            this.lb_validate_sdt.Location = new System.Drawing.Point(310, 237);
+            this.lb_validate_sdt.Name = "lb_validate_sdt";
+            this.lb_validate_sdt.Size = new System.Drawing.Size(224, 28);
+            this.lb_validate_sdt.TabIndex = 16;
+            this.lb_validate_sdt.Text = "Số điện thoại chưa nhập";
+            // 
+            // lb_validate_ten
+            // 
+            this.lb_validate_ten.AutoSize = true;
+            this.lb_validate_ten.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_validate_ten.ForeColor = System.Drawing.Color.Red;
+            this.lb_validate_ten.Location = new System.Drawing.Point(310, 321);
+            this.lb_validate_ten.Name = "lb_validate_ten";
+            this.lb_validate_ten.Size = new System.Drawing.Size(226, 28);
+            this.lb_validate_ten.TabIndex = 17;
+            this.lb_validate_ten.Text = "Tên nhân viên chưa nhập";
+            // 
+            // lb_validate_diachi
+            // 
+            this.lb_validate_diachi.AutoSize = true;
+            this.lb_validate_diachi.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_validate_diachi.ForeColor = System.Drawing.Color.Red;
+            this.lb_validate_diachi.Location = new System.Drawing.Point(310, 401);
+            this.lb_validate_diachi.Name = "lb_validate_diachi";
+            this.lb_validate_diachi.Size = new System.Drawing.Size(167, 28);
+            this.lb_validate_diachi.TabIndex = 18;
+            this.lb_validate_diachi.Text = "Địa chỉ chưa nhập";
+            // 
+            // lb_validate_pass
+            // 
+            this.lb_validate_pass.AutoSize = true;
+            this.lb_validate_pass.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_validate_pass.ForeColor = System.Drawing.Color.Red;
+            this.lb_validate_pass.Location = new System.Drawing.Point(310, 484);
+            this.lb_validate_pass.Name = "lb_validate_pass";
+            this.lb_validate_pass.Size = new System.Drawing.Size(190, 28);
+            this.lb_validate_pass.TabIndex = 19;
+            this.lb_validate_pass.Text = "Mật khẩu chưa nhập";
+            // 
+            // FAddNV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 653);
+            this.Controls.Add(this.lb_validate_pass);
+            this.Controls.Add(this.lb_validate_diachi);
+            this.Controls.Add(this.lb_validate_ten);
+            this.Controls.Add(this.lb_validate_sdt);
+            this.Controls.Add(this.pl_sdt);
+            this.Controls.Add(this.pn_tennv);
+            this.Controls.Add(this.pl_diachi);
+            this.Controls.Add(this.pl_matkhau);
+            this.Controls.Add(this.pb_huy);
+            this.Controls.Add(this.pb_luu);
+            this.Controls.Add(this.pb_thongtinnv);
+            this.Name = "FAddNV";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thông Tin Nhân Viên";
+            this.Load += new System.EventHandler(this.FAddNV_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_thongtinnv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_luu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_huy)).EndInit();
+            this.pl_sdt.ResumeLayout(false);
+            this.pl_sdt.PerformLayout();
+            this.pn_tennv.ResumeLayout(false);
+            this.pn_tennv.PerformLayout();
+            this.pl_diachi.ResumeLayout(false);
+            this.pl_diachi.PerformLayout();
+            this.pl_matkhau.ResumeLayout(false);
+            this.pl_matkhau.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private TextBox tb_sdt;
+        private TextBox tb_tennv;
+        private TextBox tb_diachi;
+        private TextBox tb_matkhau;
+        private PictureBox pb_thongtinnv;
+        private PictureBox pb_luu;
+        private PictureBox pb_huy;
+        private Panel pl_sdt;
+        private Panel pn_tennv;
+        private Panel pl_diachi;
+        private Panel pl_matkhau;
+        private Label lb_validate_sdt;
+        private Label lb_validate_ten;
+        private Label lb_validate_diachi;
+        private Label lb_validate_pass;
+    }
+}
