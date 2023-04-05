23569d6ed225914ed5bc277ecfa9d8f3c1a4b82c
Sy Luan <52100061@student.tdtu.edu.vn>
2023-04-06 01:11:53 +0700

    lai

---
+++ DrugStore/BUS/KhoThuocBUS.cs
@@ -0,0 +1,35 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class KhoThuocBUS
+    {
+        public KhoThuocBUS() { }
+        public static KhoThuocBUS instance;
+        public static KhoThuocBUS Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new KhoThuocBUS();
+                }
+                return instance;
+            }
+        }
+        public DataTable getAllKhoThuoc()
+        {
+            return DAO.KhoThuocDAO.Instance.getAllKhoThuoc();
+        }
+        public bool insertKhoThuoc(String sodk, String maphieu, string hsd, int soluong)
+        {
+            return DAO.KhoThuocDAO.Instance.insertKhoThuoc(sodk, maphieu, hsd, soluong);
+        }
+    }
+}
---
+++ DrugStore/DAO/KhoThuocDAO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class KhoThuocDAO
+    {
+        private KhoThuocDAO() { }
+        public static KhoThuocDAO instance;
+        public static KhoThuocDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new KhoThuocDAO();
+                }
+                return instance;
+            }
+        }
+        public DataTable getAllKhoThuoc()
+        {
+            return DataProvider.Instance.ExecuteQuery("SELECT Thuoc.SODK, Thuoc.TENTHUOC, MAPHIEU, HSD, SOLUONG FROM KhoThuoc JOIN Thuoc ON Thuoc.SODK = KhoThuoc.SODK");
+        }
+        public bool insertKhoThuoc(String sodk, String maphieu, string hsd, int soluong)
+        {
+            String query = string.Format("INSERT INTO KhoThuoc VALUES('{0}', '{1}', '{2}', {3})", sodk, maphieu, hsd, soluong);
+            return DataProvider.Instance.ExecuteNonQuery(query) > 0;
+        }
+    }
+}
